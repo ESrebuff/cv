8c50015d5f6cd4ff33c6a611bc71040d18cf414c
Esteban <estebanianis@gmail.com>
2024-04-04 01:09:08 +0200

    images

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.scss"; // Assurez-vous que le chemin soit correct
 import PersistentDrawerLeft from "./components/drawer";
-import hero from "./assets/images/hero1.png";
 
 interface SectionProps {
   title: string;
@@ -9,90 +8,173 @@ interface SectionProps {
 
 export default function App() {
   return (
-    <PersistentDrawerLeft>
-      <div className="hero">
-        <img src={hero} alt="" />
-        <header className="cv-header">
-          <h1>Esteban Rebuffé-Mareau</h1>
-          <p>Développeur web</p>
-        </header>
-      </div>
-      <div className="cv-container">
-        <div className="cv">
-          <MainSection title="Objectif">
-            <p>
-              Je suis passionné par le développement web et les technologies
-              full-stack, avec une expérience initiale en développement
-              front-end obtenue au cours de trois années d'alternance. Curieux
-              et désireux d'apprendre, je cherche à intégrer une équipe
-              dynamique où je pourrais mettre à profit mes compétences tout en
-              continuant à évoluer professionnellement.
-            </p>
-          </MainSection>
+    <div>
+      <div className="background"></div>
+      <PersistentDrawerLeft>
+        <div className="hero">
+          <header className="header">
+            <h1>Esteban Rebuffé-Mareau</h1>
+            <p>Développeur web</p>
+          </header>
+        </div>
+        <div className="cv-container">
+          <div className="cv">
+            <MainSection title="Objectif">
+              <p>
+                Je suis passionné par le développement web et les technologies
+                full-stack, avec une expérience initiale en développement
+                front-end obtenue au cours de trois années d'alternance. Curieux
+                et désireux d'apprendre, je cherche à intégrer une équipe
+                dynamique où je pourrais mettre à profit mes compétences tout en
+                continuant à évoluer professionnellement.
+              </p>
+            </MainSection>
 
-          <Section title="Éducation">
-            <ul>
-              <li>
-                Développeur Full-Stack - Java et Angular, Openclassrooms, 2023 -
-                2024
-              </li>
-              <li>
-                Formation Expert en Systèmes d'Information, 3W ACADEMY, 2022 -
-                2023
-              </li>
-              <li>
-                Concepteur Développeur d'Application Java Fullstack, Diginamic,
-                2021 - 2022
-              </li>
-              <li>Développeur Web, Openclassrooms, 2019 - 2021</li>
-            </ul>
-          </Section>
-
-          <Section title="Expérience Professionnelle">
-            <div>
-              <h3>Développeur Front-End, Matchers, 2021 - 2024</h3>
+            <Section title="Éducation">
               <ul>
                 <li>
-                  Automatisation de l’envoi d’emails et gestion de documents via
-                  Google Sheets et AppScript.
+                  Développeur Full-Stack - Java et Angular, Openclassrooms, 2023
+                  - 2024
                 </li>
                 <li>
-                  Développement de pages web en Vue + Nuxt et React + Next,
-                  intégration de maquettes, création de formulaires et éléments
-                  visuels.
+                  Formation Expert en Systèmes d'Information, 3W ACADEMY, 2022 -
+                  2023
                 </li>
                 <li>
-                  Tests end-to-end avec Cypress, création de mocks, et
-                  utilisation de méthodologies Agile (Kanban, Scrum).
+                  Concepteur Développeur d'Application Java Fullstack,
+                  Diginamic, 2021 - 2022
                 </li>
+                <li>Développeur Web, Openclassrooms, 2019 - 2021</li>
               </ul>
-            </div>
-          </Section>
+            </Section>
 
-          <Section title="Compétences Techniques">
-            <p>
-              HTML, CSS, SCSS, JavaScript, TypeScript, Node.js, Java, PHP, SQL,
-              Express, Spring, Angular, Vue.js, Nuxt.js, React, Next.js, Google
-              AppScript, Cypress, MUI, Tailwind CSS, SCRUM, CI/CD
-            </p>
-          </Section>
+            <Section title="Expérience Professionnelle">
+              <div>
+                <h3>Développeur Front-End, Matchers, 2021 - 2024</h3>
+                <ul>
+                  <li>
+                    Automatisation de l’envoi d’emails et gestion de documents
+                    via Google Sheets et AppScript.
+                  </li>
+                  <li>
+                    Développement de pages web en Vue + Nuxt et React + Next,
+                    intégration de maquettes, création de formulaires et
+                    éléments visuels.
+                  </li>
+                  <li>
+                    Tests end-to-end avec Cypress, création de mocks, et
+                    utilisation de méthodologies Agile (Kanban, Scrum).
+                  </li>
+                </ul>
+              </div>
+            </Section>
 
-          <Section title="Langues">
-            <p>
-              Anglais : Courant mais imparfait (Niveau B2 en 2019, aujourd'hui
-              courant)
-            </p>
-          </Section>
+            <Section title="Compétences Techniques">
+              <p>
+                HTML, CSS, SCSS, JavaScript, TypeScript, Node.js, Java, PHP,
+                SQL, Express, Spring, Angular, Vue.js, Nuxt.js, React, Next.js,
+                Google AppScript, Cypress, MUI, Tailwind CSS, SCRUM, CI/CD
+              </p>
+            </Section>
 
-          <Section title="Centres d'Intérêt">
-            <p>
-              Voyage, apprentissage continu, travail d'équipe, curiosité
-              technologique.
-            </p>
-          </Section>
+            <Section title="Langues">
+              <p>
+                Anglais : Courant mais imparfait (Niveau B2 en 2019, aujourd'hui
+                courant)
+              </p>
+            </Section>
+
+            <Section title="Centres d'Intérêt">
+              <p>
+                Voyage, apprentissage continu, travail d'équipe, curiosité
+                technologique.
+              </p>
+            </Section>
+          </div>
         </div>
-      </div>
-    </PersistentDrawerLeft>
+        <div className="hero">
+          <header className="header">
+            <h1>Esteban Rebuffé-Mareau</h1>
+            <p>Développeur web</p>
+          </header>
+        </div>
+        <div className="cv-container">
+          <div className="cv">
+            <MainSection title="Objectif">
+              <p>
+                Je suis passionné par le développement web et les technologies
+                full-stack, avec une expérience initiale en développement
+                front-end obtenue au cours de trois années d'alternance. Curieux
+                et désireux d'apprendre, je cherche à intégrer une équipe
+                dynamique où je pourrais mettre à profit mes compétences tout en
+                continuant à évoluer professionnellement.
+              </p>
+            </MainSection>
+
+            <Section title="Éducation">
+              <ul>
+                <li>
+                  Développeur Full-Stack - Java et Angular, Openclassrooms, 2023
+                  - 2024
+                </li>
+                <li>
+                  Formation Expert en Systèmes d'Information, 3W ACADEMY, 2022 -
+                  2023
+                </li>
+                <li>
+                  Concepteur Développeur d'Application Java Fullstack,
+                  Diginamic, 2021 - 2022
+                </li>
+                <li>Développeur Web, Openclassrooms, 2019 - 2021</li>
+              </ul>
+            </Section>
+
+            <Section title="Expérience Professionnelle">
+              <div>
+                <h3>Développeur Front-End, Matchers, 2021 - 2024</h3>
+                <ul>
+                  <li>
+                    Automatisation de l’envoi d’emails et gestion de documents
+                    via Google Sheets et AppScript.
+                  </li>
+                  <li>
+                    Développement de pages web en Vue + Nuxt et React + Next,
+                    intégration de maquettes, création de formulaires et
+                    éléments visuels.
+                  </li>
+                  <li>
+                    Tests end-to-end avec Cypress, création de mocks, et
+                    utilisation de méthodologies Agile (Kanban, Scrum).
+                  </li>
+                </ul>
+              </div>
+            </Section>
+
+            <Section title="Compétences Techniques">
+              <p>
+                HTML, CSS, SCSS, JavaScript, TypeScript, Node.js, Java, PHP,
+                SQL, Express, Spring, Angular, Vue.js, Nuxt.js, React, Next.js,
+                Google AppScript, Cypress, MUI, Tailwind CSS, SCRUM, CI/CD
+              </p>
+            </Section>
+
+            <Section title="Langues">
+              <p>
+                Anglais : Courant mais imparfait (Niveau B2 en 2019, aujourd'hui
+                courant)
+              </p>
+            </Section>
+
+            <Section title="Centres d'Intérêt">
+              <p>
+                Voyage, apprentissage continu, travail d'équipe, curiosité
+                technologique.
+              </p>
+            </Section>
+          </div>
+        </div>
+      </PersistentDrawerLeft>
+    </div>
   );
 }
 
